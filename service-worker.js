f5cdfd1da09eb6e055f2bd91a973a319dc81712e
77yvesyin <77yvesyin@gmail.com>
2026-05-28 23:43:21 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,13 @@
-const CACHE_NAME = "daka-web-v1";
+const CACHE_NAME = "daka-web-v2";
 const FILES = [
   "./",
   "./index.html",
   "./styles.css",
   "./app.js",
-  "./manifest.webmanifest"
+  "./manifest.webmanifest",
+  "./icons/icon-180.png",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
 ];
 
 self.addEventListener("install", (event) => {
