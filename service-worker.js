86b02ca79bfe3fa872800bb7c7acf3ea97c68cef
77yvesyin <77yvesyin@gmail.com>
2026-05-29 00:00:43 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "daka-web-v4";
+const CACHE_NAME = "daka-web-v5";
 const FILES = [
   "./",
   "./index.html",
@@ -6,6 +6,7 @@ const FILES = [
   "./app.js",
   "./manifest.webmanifest",
   "./favicon.ico",
+  "./apple-touch-icon.png",
   "./icon-180.png",
   "./icon-192.png",
   "./icon-512.png"
